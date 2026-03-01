8a1a9482e31efb526da946924eeac62ada93e718
Adarsh Shukla <adarshshuklakota18@gmail.com>
2026-03-01 23:28:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-const toggleBtn = document.getElementById("themeToggle");
-const body = document.body;
+function redirectDashboard() {
+  const role = document.getElementById("role");
 
-// Load saved theme
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add("dark-mode");
-  toggleBtn.textContent = "☀️";
-}
-
-toggleBtn.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-
-  if (body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-    toggleBtn.textContent = "☀️";
+  if (role && role.value === "student") {
+    window.location.href = "dashboards/student-dashboard.html";
+  } else if (role && role.value === "client") {
+    window.location.href = "dashboards/client-dashboard.html";
   } else {
-    localStorage.setItem("theme", "light");
-    toggleBtn.textContent = "🌙";
+    window.location.href = "dashboards/student-dashboard.html";
   }
-});
+}
