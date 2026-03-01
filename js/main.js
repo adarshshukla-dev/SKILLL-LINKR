211d5a6001c402962b18eea9eb691be52fbb4428
Adarsh Shukla <adarshshuklakota18@gmail.com>
2026-03-01 17:57:17 +0530

    Add placeholder login and registration functions

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function login() {
+  alert("Login functionality will be connected to backend later.");
+}
+
+function register() {
+  alert("Registration functionality will be connected to backend later.");
+}
